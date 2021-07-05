fc6e34770484a74d95e07922431cbe8f0f258192
Ankybasu <84119987+Ankybasu@users.noreply.github.com>
2021-07-05 18:55:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ def predict():
     return render_template('index.html', prediction_text=result)
 
 if __name__ == '__main__':
-    app.run(debug=True,use_reloader=False, port=9800)
+    app.run(debug=True)

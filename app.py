5ea1fec3f58268b64001891592df88e901f544a1
Ankybasu <84119987+Ankybasu@users.noreply.github.com>
2021-07-05 20:34:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,3 +38,5 @@ def predict():
 
 if __name__ == '__main__':
     app.run(debug=True,use_reloader=False, port=9800)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)

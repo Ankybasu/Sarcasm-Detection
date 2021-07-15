f2e209fd978ea866491e86f239a31f51ec2f8d8c
Ankybasu <84119987+Ankybasu@users.noreply.github.com>
2021-07-15 14:34:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask, render_template, request
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import PassiveAggressiveClassifier
 import pickle
-import numpy as np
-import pandas as pd
-from sklearn.model_selection import train_test_split
+
 
 app = Flask(__name__)
-loaded_model = pickle.load(open('smodel.pkl', 'rb'))
+loaded_model = pickle.load(open('lrmodel.pkl', 'rb'))
+text_transformer=pickle.load(open('transformer.pkl', 'rb'))
 
 def requestResults(result):
     if result == 0:
@@ -16,14 +13,8 @@ def requestResults(result):
         return "Sarcastic"
     
 def sardet(text): 
-    tfvect = TfidfVectorizer(stop_words='english', ngram_range=(1, 2), lowercase=True, max_features=150000)
-    dataframe = pd.read_csv("train-balanced-sarcasm.csv")
-    x = tfvect.fit_transform(dataframe['comment'].apply(lambda x: np.str_(x)))
-    y = dataframe['label']
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
     input_data = [text]
-    vectorized_input_data =  tfvect.transform(input_data)
-    prediction = loaded_model.predict(vectorized_input_data)
+    prediction = loaded_model.predict(text_transformer.transform(input_data))
     return prediction
 
 @app.route('/')
@@ -38,4 +29,4 @@ def predict():
     return render_template('index.html', prediction_text=result)
 
 if __name__ == '__main__':
-    app.run(debug=True,threaded=True)
+    app.run(debug=True,use_reloader=False)
